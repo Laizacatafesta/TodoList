2631909505718014a4e6a03fce44cde9e3be9b54
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2023-09-21 15:02:33 -0300

    adicionado key id

--- todo-react/src/App.jsx
+++ todo-react/src/App.jsx
@@ -33,7 +33,7 @@ function App() {
     <h1>Lista de Tarefas</h1>
       <div className="todo-list">
         {todos.map((todo) => ( //percorre a lista to-do
-          <Todo todo={todo}/>
+          <Todo  key={todo.id} todo={todo}/>
         ))} 
       </div>
       <TodoForm />
