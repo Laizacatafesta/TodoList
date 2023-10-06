12a199370dbaeec87c8201d5ccb85230ed71ef5e
Laiza Catafesta <72575967+Laizacatafesta@users.noreply.github.com>
2023-10-05 22:37:38 -0300

    adicionado line-through ao completar a tarefa

--- todo-react/src/components/Todo.jsx
+++ todo-react/src/components/Todo.jsx
@@ -2,7 +2,8 @@ import React from "react";
 
 const Todo = ({todo, removeTodo, completeTodo}) => {
     return (
-        <div className="todo">
+        <div className="todo" 
+        style={{textDecoration: todo.isCompleted ? "line-through" : ""}}>
             <div className="content">
               <p>{todo.text}</p> 
               <p className="category">({todo.category})</p>
